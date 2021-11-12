523c6bbb4b0d17435e7cdd41dd0a4eb836aef0de
Aswin nk <nkaswin10@gmail.com>
2021-11-12 12:12:29 +0530

    added database and logic adapter

--- chatbot.py
+++ chatbot.py
@@ -3,8 +3,21 @@ from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Creating ChatBot Instance
-chatbot = ChatBot('CoronaBot')
-
+chatbot = ChatBot(
+    'CoronaBot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+) 
  # Training with Personal Ques & Ans 
 conversation = [
     "Hello",
